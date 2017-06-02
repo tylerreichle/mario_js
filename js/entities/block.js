0efdaa97dff28e0688e1450cf8899cfa2b3e1781
Tyler <tyler.reichle@gmail.com>
2017-06-01 20:10:08 -0700

    Add coin sprite above block on hit

--- js/entities/block.js
+++ js/entities/block.js
@@ -1,5 +1,6 @@
 import Entity from './entity';
 import Sprite from './sprite';
+import Coin from './coin';
 
 class Block extends Entity {
   constructor(coin, tileset, xPos, yPos, width, height) {
@@ -7,19 +8,25 @@ class Block extends Entity {
     super('block', sprite, xPos, yPos, width, height);
 
     this.coin = coin;
-    this.coinSprite = new Sprite(tileset, 436, 21, 15, 15);
     this.coinSound = new Audio('./assets/audio/sounds/coin.wav');
     this.used = new Sprite(tileset, 486, 0, 18, 18);
+    this.tileset = tileset;
   }
 
-  // drawCoin(ctx) {
-  //   ctx.drawImage(
-  //     this.coinSprite.img,
-  //     this.coinSprite.srcX, this.coinSprite.srcY,
-  //     this.coinSprite.srcW, this.coinSprite.srcH,
-  //     this.xPos, this.yPos - 15, 15, 15
-  //   );
-  // }
+  drawCoin(data) {
+    const coins = data.entities.coins;
+    const coin = new Coin(
+      this.tileset, this.xPos - 2, this.yPos - 18, 18, 18
+    );
+
+    coin.currentState = coin.states.blockCoin;
+    coins.push(coin);
+
+    setTimeout(() => {
+      const index = coins.indexOf(coin);
+      delete coins[index];
+    }, 50);
+  }
 }
 
 export default Block;
